b915130b28bd6efb31efe4a1d390d82f4e80bac0
万里 <920113370213@njust.edu.cn>
2024-07-16 22:52:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    port: 3000, // 服务器监听端口
-    appkey: "XXXXXXXXXXXXXXXXXXXXX" // 套套/人人打码用户appkey
+    port: 23333, // 服务器监听端口
+    appkey: 'XXXXXXXXXXXXXXXXXXXXX', // 套套/人人打码用户appkey
+    referer: 'https://webstatic.mihoyo.com' //验证码所在的页面URL
 };
